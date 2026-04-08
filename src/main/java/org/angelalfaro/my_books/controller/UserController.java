ee46fa9fd9064cf0d9341cb4fa873c7e51946327
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-04-08 13:57:48 -0600

    feat: adding UserController

---
+++ src/main/java/org/angelalfaro/my_books/controller/UserController.java
@@ -0,0 +1,43 @@
+package org.angelalfaro.my_books.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.angelalfaro.my_books.entity.User;
+import org.angelalfaro.my_books.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.registerUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "auth/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, Model model) {
+        User user = userService.login(username);
+        if (user != null) {
+            // Passing the ID via redirect to simulate a session
+            return "redirect:/books?userId=" + user.getId();
+        }
+        model.addAttribute("error", "Invalid username");
+        return "auth/login";
+    }
+}
