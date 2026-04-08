08ee6fcbae968625d741f602b915b744df582816
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-04-08 13:39:00 -0600

    feat: addingUser Entity

---
+++ src/main/java/org/angelalfaro/my_books/entity/User.java
@@ -0,0 +1,22 @@
+package org.angelalfaro.my_books.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String phoneNumber;
+    
+}
