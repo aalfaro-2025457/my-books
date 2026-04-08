56c5dcdae166fdf2122e8fcb463913167079bd53
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-04-08 13:48:39 -0600

    feat: adding BookService

---
+++ src/main/java/org/angelalfaro/my_books/service/BookService.java
@@ -0,0 +1,29 @@
+package org.angelalfaro.my_books.service;
+
+import lombok.RequiredArgsConstructor;
+import org.angelalfaro.my_books.entity.Book;
+import org.angelalfaro.my_books.repository.BookRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        // Business logic to retrieve all registered books
+        return bookRepository.findAll();
+    }
+
+    public void saveBook(Book book) {
+        // Business logic to persist a new book
+        bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        // Business logic to remove a book by its ID
+        bookRepository.deleteById(id);
+    }
+}
